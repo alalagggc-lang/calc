991c892370a441e7f1c7d3b2c81e2621621de2a4
Danya14 <dadada@gmail.com>
2026-04-16 19:57:47 +0300

    V1

---
+++ ConsoleApp39/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите пример: ");
+string input = Console.ReadLine().Replace(" ", ""); 
+
+char[] ops = { '+', '-', '*', '/', '^' };
+List<double> numbers = input.Split(ops).Select(double.Parse).ToList();
+List<char> operators = input.Where(c => ops.Contains(c)).ToList();
+double result = numbers[0];
+
+for (int i = 0; i < operators.Count; i++)
+{
+    double nextNumber = numbers[i + 1];
+    char op = operators[i];
+
+    
+    if (op == '+') result += nextNumber;
+    else if (op == '-') result -= nextNumber;
+    else if (op == '*') result *= nextNumber;
+    else if (op == '/') result /= nextNumber;
+    else if (op == '^') result = Math.Pow(result, nextNumber);
+}
+
+Console.WriteLine($"= {result}");
+Console.ReadKey();
